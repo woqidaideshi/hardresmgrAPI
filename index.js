9891b256f3d214d3d6ed0528bb23de9d90d13891
LiangGuanyu <lianggy0719@126.com>
2015-05-22 11:04:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+exports.app = function() {
+  return require('./lib/app');
+}
+
+exports.app_remote = function() {
+  return require('./lib/app_remote');
+}
+
+exports.data = function() {
+  return require('./lib/data');
+}
+
+exports.resourceMgr = function() {
+  return require('./lib/resourceMgr');
+}
+
